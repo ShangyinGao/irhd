2f6f69125604cb00b2f7e0d3da32d5b2e04b6c5e
Shangyin Gao <shangyingao@dhcp-10-180-19-37.dynamic.eduroam.mwn.de>
2017-11-16 10:53:57 +0100

    fix bug

--- irhd-hw1/assignment1.py
+++ irhd-hw1/assignment1.py
@@ -1,7 +1,17 @@
+# team number: 8
+# team member: Shangyin Gao, Hanbo Hu, Jia Hua, Siyuan Liu, Xianrui Niu
+
+# Answer
+# 
+# If dimension p is very large, the angle between two randomly sampled vectors 
+# is 90 degrees.
+# 
+# the result didn't change with the increase of sample size
+
 import numpy as np
 import matplotlib.pyplot as plt
-# import pandas as pd
 import math
+import time
 
 # Task 2
 def angle(v1, v2):
@@ -18,6 +28,7 @@ def angle(v1, v2):
 
 def min_angle(array, idx):
     """
+    calculate the min angle between the idx-th vector and others
     array in 2-D form, which include all vectors
     idx is index of one vector in array
     """
@@ -29,21 +40,30 @@ def min_angle(array, idx):
 def min_angles_mean(array):
     """
     array in 2-D form
-    return a 1-D array with min_angle
+    return a number with mean of the min_angle for each idx
     """
-    min_angles = [min_angle(X, idx) for idx in range(X.shape[0])]
-#     print(len(min_angles))
+    min_angles = [min_angle(array, idx) for idx in range(array.shape[0])]
     res = np.mean(min_angles)
     return res
 
 min_angles_means = []
+# number of samples
 n_sample = 100
+# the max number of dimensions
 dim = 100
 
+st = time.time()
 for i in range(dim-1):
-    X = np.random.rand(n_sample, i+1)
+    # sample 100 vectors from dimension 1 to 1000
+    X = np.random.rand(n_sample, i+1)*2-1
+    # calcuate min_mean for each dimension
     min_mean = min_angles_mean(X)
+    # build a 1-D array with i-th number min_angles_mean for i-th dimension
     min_angles_means.append(min_mean)
+et = time.time()
+print('the time cost is:', et-st)
+# y is a idx for plot
 y = range(len(min_angles_means))
+# plot
 plt.plot(y, min_angles_means)
 plt.show()
